12b46b52cf89a2173c60611bf9d48fe8696adb7d
Hans-Pascal Beugre <hnsbeugre@protonmail.com>
2022-03-26 23:09:02 +0000

    new

--- src/applicationView/infoController.java
+++ src/applicationView/infoController.java
@@ -1,57 +1,73 @@
 package applicationView;
 
+import com.g3.dao.*;
+import com.g3.model.Etudiants;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import com.g3.dao.*;
 import javafx.scene.control.TableColumn;
 
 
 
 public class infoController implements Initializable {
 	
-	 @FXML
+
+	EtudiantsDao hans = new EtudiantsDao();
+	Etudiants h = new Etudiants();
+	//hans.getAllEtudiants();
+	
+	List < Etudiants > c = hans.getAllEtudiants();
+	@FXML
 	    private Button IdQuitter;
 
 	    @FXML
 	    private Button IdSupprimer;
 
 	    @FXML
-	    private TableColumn<?, ?> NomtblOneLicenceOne;
+	    private TableColumn<Etudiants, String> NomtblOneLicenceOne;
 
 	    @FXML
-	    private TableColumn<?, ?> NomtblThreeLicenceThree;
+	    private TableColumn<Etudiants, String> NomtblThreeLicenceThree;
 
 	    @FXML
-	    private TableColumn<?, ?> NomtblTwoLicenceTwo;
+	    private TableColumn<Etudiants, String> NomtblTwoLicenceTwo;
 
 	    @FXML
-	    private TableColumn<?, ?> PrenomtblOneLicenceOne;
+	    private TableColumn<Etudiants, String> PrenomtblOneLicenceOne;
 
 	    @FXML
-	    private TableColumn<?, ?> PrenomtblThreeLicenceThree;
+	    private TableColumn<Etudiants, String> PrenomtblThreeLicenceThree;
 
 	    @FXML
-	    private TableColumn<?, ?> PrenomtblTwoLicenceTwo;
+	    private TableColumn<Etudiants, String> PrenomtblTwoLicenceTwo;
 
 	    @FXML
 	    private Button idModifier;
 
 	    @FXML
-	    private TableView<?> tblOneLicenceOne;
+	    private TableView<Etudiants> tblOneLicenceOne;
 
 	    @FXML
-	    private TableView<?> tblThreeLicenceThree;
+	    private TableView<Etudiants> tblThreeLicenceThree;
 
 	    @FXML
-	    private TableView<?> tblTwoLicenceTwo;
+	    private TableView<Etudiants> tblTwoLicenceTwo;
 
 	    @FXML
 	    void openModifier(ActionEvent event) {
@@ -85,12 +101,30 @@ public class infoController implements Initializable {
 	
 
 	
-	
-	
+	    private void init(){
+
+			NomtblOneLicenceOne.setCellValueFactory(new PropertyValueFactory<>("noms"));
+			PrenomtblOneLicenceOne.setCellValueFactory(new PropertyValueFactory<>("prenoms"));
+			
+			
+			NomtblTwoLicenceTwo.setCellValueFactory(new PropertyValueFactory<>("noms"));
+			PrenomtblTwoLicenceTwo.setCellValueFactory(new PropertyValueFactory<>("prenoms"));
+			
+			NomtblThreeLicenceThree.setCellValueFactory(new PropertyValueFactory<>("noms"));
+			PrenomtblThreeLicenceThree.setCellValueFactory(new PropertyValueFactory<>("prenoms"));
+		 }
+	ObservableList<Etudiants> oblist = FXCollections.observableArrayList();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		init();
+		EtudiantsDao hans = new EtudiantsDao();
+		Etudiants h = new Etudiants();
+
+		tblOneLicenceOne.getItems().setAll(hans.getAllEtudiants());
+		tblTwoLicenceTwo.getItems().setAll(hans.getAllEtudiants());
+		tblThreeLicenceThree.getItems().setAll(hans.getAllEtudiants());
 		
 	}
 
